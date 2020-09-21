0695aaec1e7c0f18c6c1f3f13e43a9a45bc81079
asalmero <71348959+asalmero@users.noreply.github.com>
2020-09-21 13:39:58 -0500

    CREACION PROYECTO

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+
+namespace AplicacionPrueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int number, i, j, count = 1;
+            Console.Write("Rows :");
+            number = int.Parse(Console.ReadLine());
+            count = number - 1;
+            for (j = 1; j <= number; j++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count--;
+                for (i = 1; i <= 2 * j - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+          
+
+        }
+    }
+   
+}
